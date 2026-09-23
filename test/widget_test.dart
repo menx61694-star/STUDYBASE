0d208e17716476e9e7419c8f9996a5f4eb9a7e1d
PIXEL FORGE <menx61694@gmail.com>
2026-09-23 11:19:31 +0530

    test: validate StudyBase launcher asset

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,16 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:studybase/main.dart';
 
 void main() {
+  test('launcher source is a registered PNG asset', () async {
+    final data = await rootBundle.load('assets/branding/studybase_launcher.png');
+    final bytes = data.buffer.asUint8List();
+
+    expect(bytes.length, greaterThan(32));
+    expect(bytes.sublist(0, 8), <int>[137, 80, 78, 71, 13, 10, 26, 10]);
+  });
+
   testWidgets('StudyBase brand foundation renders', (WidgetTester tester) async {
     await tester.pumpWidget(const StudyBaseApp());
 
